eeaa37ae4a7381930eb0392d27353288dc6ffdcf
Mike <pisarenco.mihail@elev.cihcahul.md>
2024-02-21 11:10:57 +0300

    Change index location

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,9 +5,9 @@
 @section('content')
 <form action="{{route('tasks.store')}}" method="post">
         @csrf
-        <input type="text" name="name" id="Name" placeolder="Nume">
-        <input type="text" name="description" id="Description" placeholder="Descriere">
-
+        <input type="text" name="nume" id="Name" placeolder="Nume">
+        <input type="text" name="descriere" id="Descriere" placeholder="Descriere">
+        
         <button type="submit">Save</button>
     </form>
 @endsection
