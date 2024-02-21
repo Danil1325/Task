62588a49bff81a2f11b4ee8037b6d6a84125ea30
Mike <pisarenco.mihail@elev.cihcahul.md>
2024-02-21 10:28:07 +0300

    Edited the create file

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,7 +1,13 @@
 @extends('layouts.app')
 
-@section('title', 'Page Title')
+@section('title', 'Create Task')
 
 @section('content')
-    <p>This is my body content.</p>
+<form action="{{route('tasks.store')}}" method="post">
+        @csrf
+        <input type="text" name="name" id="Name" placeolder="Nume">
+        <input type="text" name="description" id="Description" placeholder="Descriere">
+
+        <button type="submit">Save</button>
+    </form>
 @endsection
