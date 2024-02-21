df737fb73d5b536141d5fab11581e09bfadb935b
Mike <pisarenco.mihail@elev.cihcahul.md>
2024-02-21 11:47:00 +0300

    Add Options

--- resources/views/task/create.blade.php
+++ resources/views/task/create.blade.php
@@ -7,7 +7,7 @@
         @csrf
         <input type="text" name="nume" id="Name" placeolder="Nume">
         <input type="text" name="descriere" id="Descriere" placeholder="Descriere">
-        
+        <input type="option">
         <button type="submit">Save</button>
     </form>
 @endsection
