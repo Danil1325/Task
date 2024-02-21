86f7cf710cbc86fa153fd538491d9194cc1d3d40
Mihail3224 <bagrinmihai5@gmail.com>
2024-02-21 13:06:43 +0200

    add store and create method

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/', function () { return view('index');})->name('index');
-
 Route::resource('tasks', TaskController::class);
+
+Route::get('/task/create', [TaskController::class, 'create'])->name('task.create');
+Route::post('/task', [TaskController::class, 'store'])->name('task.store');
