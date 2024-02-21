082938b36a1ae12a9fc6bae99985bc9389d6009d
Dumi <dumidumitrita05@gmail.com>
2024-02-21 09:19:29 +0200

    modify layouts

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,7 @@
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('/', function () { return view('index');})->name('index');
+
 Route::resource('tasks', TaskController::class);
