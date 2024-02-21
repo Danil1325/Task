1982b91648d99ff59bdb9ab63e2ffe2601794aaf
Danil1325 <danil.strelciuc@gmail.com>
2024-02-21 10:40:34 +0200

    conflict

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,5 @@ use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 
-<<<<<<<<< Temporary merge branch 1
-Route::resource('tasks', TaskController::class);
-=========
-
 Route::resource('task', TaskController::class);
->>>>>>>>> Temporary merge branch 2
+
