af88935b67cb0a3f253675e16d3c884c7997c8db
Mike <pisarenco.mihail@elev.cihcahul.md>
2024-02-21 13:22:46 +0300

    Make the main Point

--- resources/views/task/create.blade.php
+++ resources/views/task/create.blade.php
@@ -3,11 +3,11 @@
 @section('title', 'Create Task')
 
 @section('content')
-<form action="{{route('tasks.store')}}" method="post">
+<form action="{{route('task.store')}}" method="post">
         @csrf
         <input type="text" name="nume" id="Name" placeolder="Nume">
         <input type="text" name="descriere" id="Descriere" placeholder="Descriere">
-        <input type="option">
+        <input type="option" name="stare">
         <button type="submit">Save</button>
     </form>
 @endsection
